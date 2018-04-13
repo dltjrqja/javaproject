b1cbe69965a0bc643c10b0c3969c185f094ad3e5
USER <USER@INET-14>
2018-04-13 11:40:10 +0900

    sex

--- 413/src/Shape1.java
+++ 413/src/Shape1.java
@@ -2,7 +2,7 @@
 abstract class Shape1 {
 	private int x, y;
 
-	public void move(int x, int y)
+	public void move(int x, int y){
 	this.x = x;
 	this.y = y;
 }
@@ -10,13 +10,18 @@ abstract class Shape1 {
 	public abstract void draw();
 };
 
+class Rectangle extends Shape {
+	public Rectangle(int x, int y) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
+	}
 
 	private int width, height;
 
 	public void draw() {
 		System.out.println("사각형 그리기 메소드");
 	}
-
+};
 class Circle extends Shape {
 	public Circle(int x, int y) {
 		super(x, y);
@@ -26,5 +31,5 @@ class Circle extends Shape {
 	public void draw() {
 		System.out.println("원 그리기 메소드");
 	}
-}
+};
 	
