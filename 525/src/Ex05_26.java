957c1ff3e5effcdb3dd9680990b32f1830bb82b1
dltjrqja <32162622+dltjrqja@users.noreply.github.com>
2018-05-25 20:35:19 +0900

    cm
    cm

--- 525/src/Ex05_26.java
+++ 525/src/Ex05_26.java
@@ -1,6 +1,31 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 
+ 
 public class Ex05_26 {
-
+	static class MyGUI extends JFrame{
+		MyGUI(){
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setTitle("이벤트 처리 3");
+			this.setLayout(new FlowLayout());
+			
+			JTextField txt = new JTextField(10);
+			JTextArea area = new JTextArea(10, 10);
+			this.add(txt);
+			this.add(area);
+			
+			txt.addKeyListener(new KeyAdapter(){
+				public void keyReleased(KeyEvent e){
+					int key = e.getKeyCode();
+					if(key == KeyEvent.VK_ENTER){
+						String str = txt.getText();
+						
+					}
+				}
+			}
+		}
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
